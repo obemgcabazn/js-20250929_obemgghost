5d909a09e4b46b8e24aa1a5d59dca45895f55acb
obemgcabazn <obemgghost@ya.ru>
2025-09-30 11:22:42 +0300

    feat: task solved 02-js-data-types/3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,6 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let objArr = Object.entries(obj);
+  return Object.fromEntries(objArr.filter((i) => !fields.includes(i[0])));
 };
