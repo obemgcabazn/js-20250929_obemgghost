a5c47d605cdeae8794715cd80075230dd3531f48
obemgcabazn <obemgghost@ya.ru>
2025-10-13 11:42:47 +0300

    feat: task solved dom-document-loading/notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,43 @@
-export default class NotificationMessage {
+function createDiv(className, content = '') {
+  const div = document.createElement('div');
+  div.classList = className;
+  div.textContent = content;
+  return div;
+}
 
+export default class NotificationMessage {
+  constructor(message, {duration = 1000, type = 'success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = createDiv('notification');
+    this.element.classList.add(this.type);
+
+    const timer = createDiv('timer');
+    this.element.append(timer);
+
+    const inner = createDiv('inner-wrapper');
+
+    const header = createDiv('notification-header', this.type);
+    inner.append(header);
+
+    const notificationBody = createDiv('notification-body', this.message);
+
+    inner.append(notificationBody);
+
+    this.element.append(inner);
+  }
+
+  show(target = document.body) {
+    target.append(this.element);
+    setTimeout(this.destroy.bind(this), this.duration);
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  remove() {
+    this.element.remove();
+  }
 }
