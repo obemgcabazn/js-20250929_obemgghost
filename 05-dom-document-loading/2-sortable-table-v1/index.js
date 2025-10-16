c9a3c9116b1a8dbbc5dd8eea6b92d2154c2f0b9f
Alexandr Khokhriakov <obemgghost@ya.ru>
2025-10-16 21:42:52 +0300

    upd

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,26 @@
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+  }
+
+  get element() {
+    return this.createTemplate();
+  }
+
+  sort(field, order) {
 
   }
-}
 
+  createHeaderElement() {
+    return ``;
+  }
+
+  createBodyElement() {
+    return ``;
+  }
+
+  createTemplate() {
+    return this.createHeaderElement() + this.createBodyElement();
+  }
+}
