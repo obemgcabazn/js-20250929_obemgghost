f196e62e10f775495aa2ed33549ad188545f41c8
obemgcabazn <obemgghost@ya.ru>
2025-10-17 17:14:14 +0300

    upd

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,3 +1,10 @@
+function createDiv(className, content = '') {
+  const div = document.createElement('div');
+  div.classList = className;
+  div.textContent = content;
+  return div;
+}
+
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
@@ -13,14 +20,46 @@ export default class SortableTable {
   }
 
   createHeaderElement() {
-    return ``;
+    const header = createDiv('sortable-table__header sortable-table__row');
+    header.dataset.element = 'header';
+
+    this.headerConfig.map((column) => {
+      const columnElement = createDiv('sortable-table__cell');
+      columnElement.dataset.id = column.id;
+      columnElement.dataset.sortable = column.sortable;
+
+      const columnTitle = document.createElement('span');
+      columnTitle.textContent = column.title;
+      columnElement.append(columnTitle);
+
+      header.append(columnElement);
+    });
+
+    return header;
   }
 
   createBodyElement() {
-    return ``;
+    const body = createDiv('sortable-table__body');
+    body.dataset.element = 'body';
+    const bodyContent = this.data.map((product) => {
+      return `<a href="/products/${product.id}" class="sortable-table__row">
+      <div class="sortable-table__cell">
+        <img class="sortable-table-image" alt="Image" src="${product.images[0].url}">
+      </div>
+      <div class="sortable-table__cell">${product.title}</div>
+      <div class="sortable-table__cell">${product.quantity}</div>
+      <div class="sortable-table__cell">${product.price}</div>
+      <div class="sortable-table__cell">${product.sales}</div>
+    </a>`;
+    });
+    body.innerHTML = bodyContent.join('');
+    return body;
   }
 
   createTemplate() {
-    return this.createHeaderElement() + this.createBodyElement();
+    const template = createDiv('sortable-table');
+    template.append(this.createHeaderElement());
+    template.append(this.createBodyElement());
+    return template;
   }
 }
