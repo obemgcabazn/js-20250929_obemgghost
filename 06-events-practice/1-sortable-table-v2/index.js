ba3fb183a8b28cbac5e8aed85a4050eabb7ab8f3
Alexandr Khokhriakov <obemgghost@ya.ru>
2025-10-22 00:00:18 +0300

    Error: sort test

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -13,7 +13,20 @@ export default class SortableTable {
     this.headerConfig = headersConfig;
     this.data = data;
     this.sorted = sorted;
+
     this.element = this.createElement();
+    this.subElements = this.getSubElements();
+    this.sort(this.sorted.id, this.sorted.order);
+
+    this.setListeners();
+  }
+
+  getSubElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+    return [...elements].reduce((acc, subElement) => {
+      acc[subElement.dataset.element] = subElement;
+      return acc;
+    }, {});
   }
 
   createElement() {
@@ -22,7 +35,7 @@ export default class SortableTable {
     return element.firstElementChild;
   }
 
-  createSortArrowElement() {
+  createSortArrowTemplate() {
     return `
         <span data-element="arrow" class="sortable-table__sort-arrow">
           <span class="sort-arrow"></span>
@@ -36,7 +49,7 @@ export default class SortableTable {
             data-sortable="${columnConfig.sortable}"
             data-order="${columnConfig.id === this.sorted.id ? this.sorted.order : 'asc'}">
         <span>${columnConfig.title}</span>
-        ${columnConfig.id === this.sorted.id ? this.createSortArrowElement() : ''}
+        ${columnConfig.id === this.sorted.id ? this.createSortArrowTemplate() : ''}
       </div>`
     )).join('');
   }
@@ -85,7 +98,7 @@ export default class SortableTable {
     `;
   }
 
-  sort(field, order) {
+  sort = (field, order) => {
     this.sorted.id = field;
     this.sorted.order = order;
 
@@ -106,8 +119,43 @@ export default class SortableTable {
     };
 
     this.data.sort(compareFn);
+    const newElement = this.createElement();
+    this.element.replaceWith(newElement);
+    this.element = newElement;
+  }
 
-    this.element = this.createElement();
-    console.log(this.data);
+  setListeners() {
+    window.addEventListener('pointerdown', this.onTableCellClick);
+  }
+
+  onTableCellClick = (event) => {
+    const tableCell = event.target.closest('.sortable-table__cell');
+    if (!tableCell || tableCell.dataset.sortable === 'false') {
+      return;
+    }
+
+    function reverseOrder(order) {
+      return order === 'asc' ? 'desc' : 'asc';
+    }
+
+    if (this.sorted.id === tableCell.dataset.id) {
+      const newSortOrder = reverseOrder(this.sorted.order);
+      tableCell.dataset.order = newSortOrder;
+      this.sorted.order = newSortOrder;
+    } else {
+      this.sorted.id = tableCell.dataset.id;
+      this.sorted.order = 'asc';
+    }
+
+    this.sort(this.sorted.id, this.sorted.order);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    window.removeEventListener('click', this.onTableCellClick);
   }
 }
